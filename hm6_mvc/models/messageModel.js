31c31eada26b265f0a9b1d796c6267ff056f4da3
Nadiia Balitska <161743763+Nadiia-balitska@users.noreply.github.com>
2025-07-06 11:43:20 +0200

    homework 6 fixed

--- hm6_mvc/models/messageModel.js
+++ hm6_mvc/models/messageModel.js
@@ -4,24 +4,27 @@ const he = require("he");
 
 const messagePath = path.join(__dirname, "../config/data.json");
 
+function getAll() {
+  if (!fs.existsSync(messagePath)) {
+    return [];
+  }
+
+  const date = fs.readFileSync(messagePath, "utf8");
+  return JSON.parse(date);
+}
+function add(username, text) {
+  const messages = getAll();
+
+  const safeMessage = {
+    safeName: he.encode(username),
+    safeText: he.encode(text),
+  };
+  messages.push(safeMessage);
+
+  fs.writeFileSync(messagePath, JSON.stringify(messages, null, 2), "utf8");
+}
+
 module.exports = {
-  getAll: () => {
-    const date = fs.readFileSync(messagePath, "utf8");
-    return JSON.parse(date);
-  },
-  add: (username, text) => {
-    const messages = getAll();
-    messages.appendFile({ username, text });
-
-    const safeMessage = {
-      safeName: he.encode(username),
-      safeText: he.encode(text),
-    };
-    messages.push(safeMessage);
-
-    fs.writeFile(messagePath, JSON.stringify(messages, null, 2), (err) => {
-      if (err) return res.status(500).send("error");
-      res.redirect("/");
-    });
-  },
+  getAll,
+  add,
 };
